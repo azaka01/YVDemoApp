b0df3165bd088d12970816ddb38135305f433467
Awais Zaka <az@intsoftdev.com>
2021-04-18 22:01:43 +0100

    add JetPack Compose for user card details

---
+++ app/src/main/kotlin/com/intsoftdev/yvdemoapp/ui/compose/UserDetailsCard.kt
@@ -0,0 +1,73 @@
+package com.intsoftdev.yvdemoapp.ui.compose
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.coil.CoilImage
+import com.intsoftdev.domain.UserDetails
+import com.intsoftdev.yvdemoapp.R
+
+@Composable
+fun UserDetailsCard(
+    userDetails: UserDetails?,
+    onClick: () -> Unit,
+) {
+    requireNotNull(userDetails)
+    Column(verticalArrangement = Arrangement.Top) {
+        Card(
+            shape = MaterialTheme.shapes.medium,
+            modifier = Modifier
+                .padding(all = 16.dp)
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .clickable(onClick = onClick),
+            elevation = 8.dp,
+        ) {
+            Row(verticalAlignment = Alignment.Top) {
+                CoilImage(
+                    data = userDetails.picUrl,
+                    previewPlaceholder = R.drawable.ic_launcher,
+                    contentDescription = "",
+                    modifier = Modifier
+                        .width(120.dp)
+                        .height(120.dp)
+                        .padding(all = 16.dp),
+                    contentScale = ContentScale.Crop,
+                )
+                Column(
+                    verticalArrangement = Arrangement.SpaceEvenly, modifier = Modifier
+                        .padding(all = 16.dp)
+                ) {
+                    Text(userDetails.displayName)
+                    Text(userDetails.email)
+                    Text(userDetails.displayAddress)
+                }
+            }
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun UserDetailsCardPreview() {
+    UserDetailsCard(
+        UserDetails(
+            displayName = "Chief Monjubi",
+            displayAddress = "Central Africa",
+            email = "chief@mail.com",
+            picUrl = "https://randomuser.me/api/portraits/thumb/men/59.jpg",
+            latitude = 0.0,
+            longitude = 0.0
+        ),
+        {}
+    )
+}
--- app/src/main/kotlin/com/intsoftdev/yvdemoapp/ui/userdetails/UserDetailsFragment.kt
+++ app/src/main/kotlin/com/intsoftdev/yvdemoapp/ui/userdetails/UserDetailsFragment.kt
@@ -1,43 +1,33 @@
 package com.intsoftdev.yvdemoapp.ui.userdetails
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import coil.load
-import com.intsoftdev.yvdemoapp.databinding.FragmentUserDetailsBinding
+import com.intsoftdev.yvdemoapp.ui.compose.UserDetailsCard
 import com.intsoftdev.yvdemoapp.viewmodel.UsersViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 
+@ExperimentalCoroutinesApi
+@AndroidEntryPoint
 class UserDetailsFragment : Fragment() {
 
     private val usersViewModel: UsersViewModel by activityViewModels()
 
-    private var _binding: FragmentUserDetailsBinding? = null
-
-    private val binding get() = _binding!!
-
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentUserDetailsBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding.userName.text = usersViewModel.selectedUser?.displayName
-        binding.userAddress.text = usersViewModel.selectedUser?.displayAddress
-        binding.userImage.load(usersViewModel.selectedUser?.picUrl)
-        binding.userEmail.text = usersViewModel.selectedUser?.email
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+        return ComposeView(requireContext()).apply {
+            setContent {
+                UserDetailsCard(userDetails = usersViewModel.selectedUser, onClick = {})
+            }
+        }
     }
 }
